e62a979e2d2dab126cfa4928ac8e1886d15de5fe
Agwe Bryan <agwedinobryan@gmail.com>
2022-09-02 01:49:50 +0100

    java-word-counter

---
+++ java-word-counter/src/WordCounter.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class WordCounter {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		// show first message to user
+		System.out.println("Type in a paragraph:");	
+		
+		// get input and remove leading and trailing spaces
+		String paragraph = (scanner.nextLine()).trim();
+		
+		// use to ensure that user types in something
+		while(paragraph.length() == 0) {
+			
+			System.out.println("Type in a paragraph");
+			
+			// get input and remove leading and trailing spaces
+			paragraph = (scanner.nextLine()).trim();
+		}
+		
+		// display paragraph to user
+		System.out.println("Your paragraph: " + paragraph);
+		
+		// variable to count number of spaces
+		int spaceCount = 0;
+		int wordCount = 0;
+		
+		for (int i = 0; i < paragraph.length(); i++) {
+			char characterAt = paragraph.charAt(i);
+			
+			// Check if current index is ' ' and 
+			// next index is not ' ' 
+			if(characterAt == ' ' && paragraph.charAt(i + 1) != ' ') {  
+				spaceCount++;
+			}
+			System.out.println(characterAt);
+			System.out.println( paragraph.charAt(i+1));
+			
+		}
+		
+		// increment again to get word count
+		wordCount = spaceCount + 1;
+		
+		System.out.println("The number of words in your paragraph is: " + wordCount);
+		
+		// close scanner
+		scanner.close();
+	}
+
+}
